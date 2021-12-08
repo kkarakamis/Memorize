87eb0a9bff150818f8e0bc881236e818c37ffaf2
Kutay Karakamış <32245278+kkarakamis@users.noreply.github.com>
2021-12-08 11:47:35 +0300

    Pie shape added

--- Memorize/Shapes/Pie.swift
+++ Memorize/Shapes/Pie.swift
@@ -6,4 +6,28 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct Pie: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var isClockwise: Bool = false
+ 
+    func path(in rect: CGRect) -> Path {
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = min(rect.width, rect.height)/2
+        let start = CGPoint(
+            x: center.x + radius * CGFloat(cos(startAngle.radians)),
+            y: center.y + radius * CGFloat(sin(startAngle.radians))
+        )
+        
+        var p = Path()
+        p.move(to: center)
+        p.addLine(to: start)
+        p.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: !isClockwise)
+        p.addLine(to: center)
+        
+        return p
+    }
+}
 
